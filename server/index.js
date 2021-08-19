eccefd0f2ba9f486fb31617f78e9614b757fafef
mayb3lat3r <zaqhaha@gmail.com>
2021-08-19 14:02:40 +0300

    added cuncurrently scripts

--- server/index.js
+++ server/index.js
@@ -4,12 +4,13 @@ const config = require('config')
 const authRouter = require('./routes/auth.routes')
 
 const app = express()
-const PORT = config.get('serverPort')
+const PORT = process.env.PORT || config.get('serverPort')
 
 app.use(express.json())
 app.use('/api/auth', authRouter)
 
 const start = async () => {
+    console.log(PORT);
     try {
         await mongoose.connect(config.get("dbUrl"), { useNewUrlParser: true , useUnifiedTopology: true, useCreateIndex: true})
 
@@ -18,7 +19,7 @@ const start = async () => {
         })
         
     } catch (e) {
-
+        
     }
 }
 
